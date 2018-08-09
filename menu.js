e3fd051fd120d32e848070ab95e56dd84ab591f9
ing-maycol28peru <37317478+ing-maycol28peru@users.noreply.github.com>
2018-08-09 13:40:43 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,21 @@
+(function($) {
+  'use strict';
+
+  var $accountDelete       = $('#delete-account'),
+    $accountDeleteDialog = $('#confirm-delete'),
+    transition;
+
+  $accountDelete.on('click', function() {
+    $accountDeleteDialog[0].showModal();
+    transition = window.setTimeout(function() {
+        $accountDeleteDialog.addClass('dialog-scale');
+    }, 0.5);
+  });
+
+  $('#cancel').on('click', function() {
+    $accountDeleteDialog[0].close();
+    $accountDeleteDialog.removeClass('dialog-scale');
+    clearTimeout(transition);
+  });
+
+})(jQuery);
